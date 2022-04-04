ccb8689226dd94a3e207f4104041bc2011ff8d8a
retpinheiro <reehpinheiro.s@gmail.com>
2022-04-03 22:32:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+for(var y = 1; y <= 10; y++) {
+
+    for(var x = 1; x <= 10; x++ ){
+        //acao
+        document.write(y + ' x ' + x + ' = ' + (y * x) + '<br>');
+    }
+
+    document.write('<hr>');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
